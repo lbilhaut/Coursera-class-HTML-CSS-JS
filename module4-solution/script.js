12a83c3acbfff30d994ef0af04883696b1730ffb
WhiteWhate <lise.bilhaut@gmail.com>
2019-10-16 11:25:23 -0400

    Strict comparison

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -61,7 +61,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (firstLetter == "j") {
+  if (firstLetter === "j") {
     byeSpeaker.speak(names[name]);
   } else {
     helloSpeaker.speak(names[name]);
